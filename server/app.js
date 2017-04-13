06a3127f8fe23a537e5adf4ec817458f1156f012
Mason Wan <mwan@linkedin.com>
2017-04-13 09:49:33 -0700

    Mark `DOMContentLoaded` logging

--- server/app.js
+++ server/app.js
@@ -14,9 +14,14 @@ app
       res.send(`console.log('Script ${req.params.id} in head, async')`)
     }, Math.random() * 500)
   })
-  .get('/body-script-:id.js', (req, res) => {
+  .get('/body-script-sync-:id.js', (req, res) => {
     setTimeout(() => {
-      res.send(`console.log('Script ${req.params.id} in body')`)
+      res.send(`console.log('Script ${req.params.id} in body, sync')`)
+    }, Math.random() * 100)
+  })
+  .get('/body-script-async-:id.js', (req, res) => {
+    setTimeout(() => {
+      res.send(`console.log('Script ${req.params.id} in body, async')`)
     }, Math.random() * 100)
   })
   .use('/', express.static(path.join(__dirname, `../public`)))
